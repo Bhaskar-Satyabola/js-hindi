3d3ffb1b95f2e6a7e2ed0a91e934e0bf5e778e80
Bhaskar-Satyabola <141653211+Bhaskar-Satyabola@users.noreply.github.com>
2023-09-25 06:22:33 +0000

    variable and datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+let name="bhaskar"
+let age=22
+let isLoggedIn=true
+let state;
+
+
+
+//number =>2 to power 53
+// bigint
+// string=""
+// boolean=true/false
+// null=standalone value
+//undefined
+//symbol=>unique
+
+
+//Object 
+
+console.log(typeof "bhaskar")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
