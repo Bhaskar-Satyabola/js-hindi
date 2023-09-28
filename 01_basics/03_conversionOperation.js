8fe3f82c3307d29db3edfdb7921edc2b3a06ff42
Bhaskar-Satyabola <141653211+Bhaskar-Satyabola@users.noreply.github.com>
2023-09-28 02:54:01 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,7 +18,7 @@ let valueInNumber=Number(score);
 let isLoggedIn= "false"
 let booleanIsLoggedIn=Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 
 //1=>true
@@ -30,5 +30,40 @@ console.log(booleanIsLoggedIn);
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+
+//----------------------operations----------------------
+
+let value=3
+let negValue=-value
+//console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1="Helloo"
+let str2=" Bhaskar"
+let str3= str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3;
+num1 = num2 = num3 = 2+2
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
