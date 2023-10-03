56f1b2ddd103fc901382303a34f7cfe45eff8ad5
Bhaskar-Satyabola <141653211+Bhaskar-Satyabola@users.noreply.github.com>
2023-10-03 06:46:49 +0000

    nums & math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,44 @@
+const score=400
+// console.log(score);
+const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber=23.8966
+// console.log(otherNumber.toPrecision(2)) //expected output 24
+// console.log(otherNumber.toPrecision(3))//expected output 23.9
+// console.log(otherNumber.toPrecision(4))//expected output 23.90
+// console.log(otherNumber.toPrecision(5))//expected output 23.897
+// console.log(otherNumber.toPrecision(6))//expected output 23.8966
+// console.log(otherNumber.toPrecision(7))//expected output 23.89660
+// console.log(otherNumber.toPrecision(8))//expected output 23.896600
+// console.log(otherNumber.toPrecision(10))//expected output 23.89660000
+
+
+const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++++++++++++++++++++MATHS+++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.7));
+// console.log(Math.floor(4.7));
+// console.log(Math.sqrt(100));
+// console.log(Math.min(100,222,334,636,2431));
+// console.log(Math.max(100,222,334,636,2431));
+
+console.log(Math.random);
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max -min +1))+min);
+
+
+
