fbe17238f5a9c52cfb5f1841f517d3334e5e9874
Bhaskar-Satyabola <141653211+Bhaskar-Satyabola@users.noreply.github.com>
2023-10-03 04:14:03 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+const name="bahsakar"
+const repoCount=50
+
+// console.log(name + repoCount+ "Value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName=new String('bhaskar-sb-bsb')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('5'));
+// console.log(gameName.indexOf('b'));
+
+const newString=gameName.substring(0,4)
+console.log(newString);
+
+const anotherString= gameName.slice(-9,4)
+console.log(anotherString);
+
+const newStringOne="   hitesh  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url="https//www.bsb.com/bhaskar%20Satyabola"
+console.log(url.replace('%20','-'));
+console.log(url.includes('bhaskar'));
+console.log(url.includes('hitesh'));
+
+console.log(gameName.split('-'));
+console.log(gameName.split(''));
+console.log(gameName.split(' '));
