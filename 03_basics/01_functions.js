dfa1b58e096ae27b718500ddefccce91dfafb7f8
Bhaskar-Satyabola <141653211+Bhaskar-Satyabola@users.noreply.github.com>
2023-10-25 08:32:08 +0000

    func

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,4 +35,36 @@ return
 return `${username} just logged in`
 }
 // console.log(loginUserMessage("Bhaskar")); 
-console.log(loginUserMessage("bhaskar")); 
+// console.log(loginUserMessage("bhaskar")); 
+
+function calculateCartPrice(val1,val2,...num1){
+return num1
+}
+
+// console.log(calculateCartPrice(200,400,500,2000))
+
+
+const user={
+    username:"bsb",
+    prices:3000
+}
+
+function handleObject(anyObject){
+console.log(`UserName is ${anyObject.username} & Price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    username:"sam",
+    price:400
+})
+
+
+const myNewArray=[200,300,400,600];
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200,3000,4000,]))
